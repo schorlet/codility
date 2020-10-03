8e37373b4ff4450514fcdb72d1694c417784ab5f
stephanec <stephanec@washingmachine>
2020-10-03 15:58:41 +0200

    cyclic_rotation

---
+++ cyclic_rotation.go
@@ -0,0 +1,17 @@
+package solution
+
+func Solution(A []int, K int) []int {
+	lenA := len(A)
+	if lenA == 0 || K == 0 {
+		return A
+	}
+	if lenA == K || K%lenA == 0 {
+		return A
+	}
+	B := make([]int, K+lenA)
+	copy(B[K:], A)
+	for i := 0; i < K; i++ {
+		B[K-1-i] = B[lenA+K-1-i]
+	}
+	return B[:lenA]
+}
